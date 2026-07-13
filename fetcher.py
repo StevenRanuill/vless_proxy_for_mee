6913ea666bebf7e3a08256e58764fb18e9398f61
StevenRanuill <sergey.corepanov25@gmail.com>
2026-07-13 12:21:58 +0700

    Update fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -288,5 +288,6 @@ async def async_main():
                 f.write("\n".join(chunk))
         log_debug("Все чанки успешно нарезаны и сохранены.")
 
- if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(async_main())
+
